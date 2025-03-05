a5e021b9e8fccc836b8698d1af47ec5306c66c41
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-04 20:08:59 -0500

    added third window

---
+++ GasotecFactureCreator/DescriptionWindowService.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace GasotecFactureCreator;
+
+public partial class DescriptionWindowService : Window
+{
+    public DescriptionWindowService()
+    {
+        InitializeComponent();
+    }
+}
