8f5c6bc34d4938e5117f698e0b4ad4c0b24b5a9d
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-06 00:34:12 -0500

    implement domain

--- GasotecFactureCreator/Presentation/ClientDataCollectorWIndow.xaml.cs
+++ GasotecFactureCreator/Presentation/ClientDataCollectorWIndow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using GasotecFactureCreator.Controller;
 
 namespace GasotecFactureCreator.Presentation;
 
@@ -18,6 +19,14 @@ public partial class ClientDataCollectorWIndow : Window
 
     private void Sig_Button_Click(Object sender, RoutedEventArgs e)
     {
+        string name = TextBoxName.Text;
+        string address = TextBoxAddres.Text;
+        int phone = int.Parse(TextBoxPhone.Text);
+        string mail = TextBoxMail.Text;
+        string nit = TextBoxNit.Text;
+
+        SalesCheckerController.CreateSalesChecker(name, address, phone, mail, nit);
+
         DescriptionWindowService service = new DescriptionWindowService();
         this.Close();
         service.Show();
