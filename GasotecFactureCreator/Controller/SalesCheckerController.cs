a582dff820e460e60cb10966c921aa554abcec78
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-06 00:33:48 -0500

    added controller salesChecker class

---
+++ GasotecFactureCreator/Controller/SalesCheckerController.cs
@@ -0,0 +1,18 @@
+﻿using GasotecFactureCreator.Domain;
+
+namespace GasotecFactureCreator.Controller;
+
+public static class SalesCheckerController
+{
+    public static SalesChecker CurrentSalesChecker { get; private set; }
+
+    public static void CreateSalesChecker(string name, string address, int phoneNumber, string email, string Nit)
+    {
+        CurrentSalesChecker = new SalesChecker(name, address, phoneNumber, email, Nit);
+    }
+
+    public static SalesChecker GetCurrentSalesChecker()
+    {
+        return CurrentSalesChecker;
+    }
+}
