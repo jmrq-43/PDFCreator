34f13f4c688939ddc681139ce9cc8c7b3842c1e2
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-11 20:32:26 -0500

    add service domain

---
+++ GasotecFactureCreator/Domain/Service.cs
@@ -0,0 +1,8 @@
+﻿namespace GasotecFactureCreator.Domain;
+
+public class Service
+{
+    public string serviceType { get; set; }
+    public string serviceDescription { get; set; }
+    public decimal servicePrice { get; set; }
+}
