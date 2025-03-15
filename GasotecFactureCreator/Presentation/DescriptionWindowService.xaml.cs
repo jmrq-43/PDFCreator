4c8c0524eebea780641b004bd2a4a9dfe158796a
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-15 01:35:11 -0500

    change

--- GasotecFactureCreator/Presentation/DescriptionWindowService.xaml.cs
+++ GasotecFactureCreator/Presentation/DescriptionWindowService.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 using GasotecFactureCreator.Controller;
 using GasotecFactureCreator.Controller.Service;
