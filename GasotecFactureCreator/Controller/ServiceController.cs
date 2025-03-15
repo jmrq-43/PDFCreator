45ac28545d35c5f2b11d09d9b4cc1c41d2ea73d2
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-15 01:37:26 -0500

    add service controller

---
+++ GasotecFactureCreator/Controller/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using GasotecFactureCreator.Domain;
+
+namespace GasotecFactureCreator.Controller;
+
+public class ServiceController
+{
+    private static List<ServiceDomain> _services = new List<ServiceDomain>();
+
+    public static void SetCurrentServices(List<ServiceDomain> services)
+    {
+        _services = services;
+    }
+
+    public static List<ServiceDomain> GetCurrentServices()
+    {
+        return _services;
+    }
+}
