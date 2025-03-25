a0e8180db0067dabcb879712f30bc35948f7e545
jmrq-43 <rojasquinteroj26@gmail.com>
2025-03-25 16:03:34 -0500

    added amount field

--- GasotecFactureCreator/Domain/ServiceDomain.cs
+++ GasotecFactureCreator/Domain/ServiceDomain.cs
@@ -5,4 +5,5 @@ public class ServiceDomain
     public string serviceType { get; set; }
     public string serviceDescription { get; set; }
     public decimal servicePrice { get; set; }
+    public int serviceAmount { get; set; }
 }
